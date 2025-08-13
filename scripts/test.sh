543958c2156689e768faf9686cf5e9b961f861a9
Aleksey <11_alx_11@ro.ru>
2025-08-13 13:00:22 +0300

    fix timeline graph finally, by eradicating wrong use of retry tab

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+rm -r ./*/allure-results/
 go test -timeout 5s ./...
 cp -r ./allure-report/history ./allure-results/ && echo "Copied history."
 allure generate ./allure-results/ ./*/allure-results/ --clean
