affdff09c75b8ffed92d9fb62d227fe1af889a25
Aleksey <11_alx_11@ro.ru>
2025-08-14 11:07:16 +0300

    add retries for tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 rm -r ./*/allure-results/
-go test -timeout 5s ./...
+go test -count 3 -timeout 5s ./...
 cp -r ./allure-report/history ./allure-results/ && echo "Copied history."
 allure generate ./allure-results/ ./*/allure-results/ --clean
