82d3085eb3aa5246c3aa19dcf211c966a5d0b6d2
Aleksey <11_alx_11@ro.ru>
2025-09-26 19:33:52 +0300

    working on writer

--- app-oop/writeout.go
+++ app-oop/writeout.go
@@ -4,20 +4,25 @@ import (
 	"github.com/11ALX11/calc-arithmetics/app"
 )
 
-// Writer represents a type that can write to a txt file.
-type Writein struct {
+// Writeout represents a type that can write to a txt file.
+type Writeout struct {
 	err error
 }
 
+// Not used with Writeout
+func (r *Writeout) SetDataInputFile(dataInputFile string) Writer {
+	return r
+}
+
 // Getter for err attribute
-func (w Writein) GetError() error {
+func (w Writeout) GetError() error {
 	return w.err
 }
 
 /*
 Same as WriteFile() in app package
 */
-func (w *Writein) WriteFile(outputFile, content string) Writer {
+func (w *Writeout) WriteFile(outputFile, content string) Writer {
 	w.err = app.WriteFile(outputFile, content)
 	return w
 }
--- app-oop/writer.go
+++ app-oop/writer.go
@@ -6,6 +6,8 @@ type Writer interface {
 	// Same as WriteFile() or WriteZipFile() in app package
 	WriteFile(outputFile, content string) Writer
 
+	// Setter for dataInputFile attribute
+	SetDataInputFile(dataInputFile string) Writer
 	// Getter for err attribute
 	GetError() error
 }
