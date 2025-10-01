e4c4395ecaec2f24dfc24e04c581205f5f76e078
Aleksey <11_alx_11@ro.ru>
2025-10-01 20:10:08 +0300

    working on decrypt as a decorator

--- app-oop/decrypt.go
+++ app-oop/decrypt.go
@@ -4,18 +4,62 @@ import (
 	"github.com/11ALX11/calc-arithmetics/app"
 )
 
-// Decrypt is a type that decrypts a ciphertext string using AES. Implements ...Decorator
+// Decrypt is a type that decrypts a ciphertext string using AES. Implements ReaderDecorator
 type Decrypt struct {
+	wrappee Reader
+
 	keyPath string
 
 	resultText string
 	resultErr  error
 }
 
-// Constructor for Decrypt
-func NewDecrypt() *Decrypt {
-	d := new(Decrypt)
-	return d
+// NewDecrypt is a constructor for a Decrypt.
+func NewDecrypt(reader Reader) *Decrypt {
+	f := new(Decrypt)
+	f.wrappee = reader
+	return f
+}
+
+// Setter for dataInputFile attribute
+func (r *Decrypt) SetDataInputFile(dataInputFile string) Reader {
+	return r.wrappee.SetDataInputFile(dataInputFile)
+}
+
+// Getter for content attribute
+func (r Decrypt) GetContent() string {
+	return r.wrappee.GetContent()
+}
+
+// Setter for content attribute
+func (r *Decrypt) SetContent(s string) Reader {
+	return r.wrappee.SetContent(s)
+}
+
+// Getter for err attribute
+func (r Decrypt) GetError() error {
+	return r.wrappee.GetError()
+}
+
+// Setter for content attribute
+func (r *Decrypt) SetError(e error) Reader {
+	return r.wrappee.SetError(e)
+}
+
+// Getter for both content and error.
+// Ex: content, err := reader.GetContentError()
+func (r Decrypt) GetContentError() (string, error) {
+	return r.wrappee.GetContentError()
+}
+
+/*
+Same as ReadFile() in app package
+*/
+func (r *Decrypt) ReadFile(inputFile string) Reader {
+
+	// ...
+
+	return r.wrappee.ReadFile(inputFile)
 }
 
 // Populate Decrypt fields with decrypted text and error if it happened.
--- app-oop/reader-decorator.go
+++ app-oop/reader-decorator.go
@@ -11,3 +11,42 @@ type ReaderDecorator interface {
 // 	f.wrappee = reader
 // 	return f
 // }
+
+// // Setter for dataInputFile attribute
+// func (r *ReaderDecorator) SetDataInputFile(dataInputFile string) Reader {
+// 	return r.wrappee.SetDataInputFile(dataInputFile)
+// }
+
+// // Getter for content attribute
+// func (r ReaderDecorator) GetContent() string {
+// 	return r.wrappee.GetContent()
+// }
+
+// // Setter for content attribute
+// func (r *ReaderDecorator) SetContent(s string) Reader {
+// 	return r.wrappee.SetContent(s)
+// }
+
+// // Getter for err attribute
+// func (r ReaderDecorator) GetError() error {
+// 	return r.wrappee.GetError()
+// }
+
+// // Setter for content attribute
+// func (r *ReaderDecorator) SetError(e error) Reader {
+// 	return r.wrappee.SetError(e)
+// }
+
+// // Getter for both content and error.
+// // Ex: content, err := reader.GetContentError()
+// func (r ReaderDecorator) GetContentError() (string, error) {
+// 	return r.wrappee.GetContentError()
+// }
+
+// /*
+// Same as ReadFile() in app package
+// */
+// func (r *ReaderDecorator) ReadFile(inputFile string) Reader {
+// 	// modify here to insert action
+// 	return r.wrappee.ReadFile(inputFile)
+// }
