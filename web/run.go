3d712c30472dfdf2308bbad5a88a1f4b7833ba1d
Aleksey <11_alx_11@ro.ru>
2025-10-20 20:19:14 +0300

    reading tutorial

--- web/run.go
+++ web/run.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 
 	"github.com/11ALX11/calc-arithmetics/i18n"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 )
 
 func Run(cmd *cobra.Command, args []string) {
 	fmt.Println(i18n.T("web not supported yet."))
+
+	router := gin.Default()
+	// router.GET("/", )
+
+	// router.Run("0.0.0.0:80")
 }
