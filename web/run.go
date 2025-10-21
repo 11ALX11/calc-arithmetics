58505e15964edbb3262a2d1b7b8d45f2d1c0fe8d
Aleksey <11_alx_11@ro.ru>
2025-10-21 19:49:07 +0300

    working with example html and router

--- web/run.go
+++ web/run.go
@@ -1,18 +1,28 @@
 package web
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/11ALX11/calc-arithmetics/i18n"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 )
 
 func Run(cmd *cobra.Command, args []string) {
-	fmt.Println(i18n.T("web not supported yet."))
-
 	router := gin.Default()
-	// router.GET("/", )
 
-	// router.Run("0.0.0.0:80")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.POST("/replace-math-expression", func(c *gin.Context) {
+		expression := c.PostForm("expression")
+		// Here you can implement your logic to replace math expressions
+		// For demonstration, let's just return the received expression
+		// result := replaceMathExpressions(expression)
+		c.JSON(http.StatusOK, gin.H{"result": expression})
+	})
+
+	router.LoadHTMLFiles("web/index.html")
+
+	router.Run("0.0.0.0:8080")
 }
