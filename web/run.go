162530cc13dc266a2dfc0ded684010c960125fc1
Aleksey <11_alx_11@ro.ru>
2025-10-23 22:07:53 +0300

    working on loading form data to flags

--- web/run.go
+++ web/run.go
@@ -3,6 +3,7 @@ package web
 import (
 	"net/http"
 
+	"github.com/11ALX11/calc-arithmetics/flags"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 )
@@ -16,6 +17,16 @@ func Run(cmd *cobra.Command, args []string) {
 
 	router.POST("/replace-math-expression", func(c *gin.Context) {
 		expression := c.PostForm("expression")
+
+		flags.Decrypt = c.PostForm("decrypt") == "on"
+		flags.Encrypt = c.PostForm("encrypt") == "on"
+		flags.KeyPath = c.PostForm("keyPath")
+		flags.Unzip = c.PostForm("unzip") == "on"
+		flags.UseEvalLib = c.PostForm("useEvalLib") == "on"
+		flags.UseFilterRegex = c.PostForm("useFilterRegex") == "on"
+		flags.Archive = c.PostForm("archive") == "on"
+		flags.DataFileInArchive = c.PostForm("dataFileInArchive")
+
 		result, err := replaceMathExpressions(expression)
 		c.JSON(http.StatusOK, gin.H{"result": result, "error": err})
 	})
--- web/runApp.go
+++ web/runApp.go
@@ -20,6 +20,7 @@ func replaceMathExpressions(expr string) (string, error) {
 	// flag: unzip
 	if flags.Unzip {
 
+		// ToDo: debug, doesnt work, error `{}` on front and no result
 		content, err = app.ReadZipData(content, flags.DataFileInArchive)
 
 		if err != nil {
